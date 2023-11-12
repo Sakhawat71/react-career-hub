e69b4cb04524a9b3d629a892853420bbba14b3b4
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-12 15:42:16 +0600

    get job data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import CatagoryList from "../CatagoryList/CatagoryList";
+import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <CatagoryList></CatagoryList>
+            <FeaturedJobs></FeaturedJobs>
         </div>
     );
 };
