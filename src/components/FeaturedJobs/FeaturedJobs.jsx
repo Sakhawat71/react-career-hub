e6891746ae83fa3710a1f5824f58288deb160cb4
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-12 18:01:41 +0600

    complete job cart

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,8 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
     const [jobs,setJobs] = useState([])
+    const [dataLength, setDataLength] = useState(4)
+
     useEffect(()=>{
         fetch('jobs.json')
         .then(res => res.json())
@@ -12,14 +14,14 @@ const FeaturedJobs = () => {
     },[])
 
     return (
-        <div>
-            <div className="text-center space-y-3 my-3">
+        <div className="my-10">
+            <div className="text-center space-y-3 my-10">
                 <h1 className="text-4xl font-extrabold text-[#1A1919]">Featured Jobs</h1>
                 <p className="text-xl font-normal text-[#757575]">Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
             </div>
         </div>
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,27 +1,33 @@
 import PropTypes from 'prop-types';
+import { IoLocationOutline } from 'react-icons/io5';
+import { BiDollarCircle } from 'react-icons/bi';
 
 const Job = ({ job }) => {
 
     const { logo, job_title, company_name, remote_or_onsite, job_type, location, salary } = job;
 
     return (
-        <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src={logo} alt="icon" /></figure>
+        <div className="card card-compact bg-base-100 shadow border my-2">
+
+            <figure className='mt-3 pt-5'>
+                <img className="" src={logo} alt="icon" />
+            </figure>
+
             <div className="card-body">
-                <h2 className="card-title">{job_title}</h2>
-                <p>{company_name}</p>
-                <div>
-                    <button>{remote_or_onsite}</button>
-                    <button>{job_type}</button>
+                <h2 className="font-extrabold text-2xl my-0 text-[#474747]">{job_title}</h2>
+                <p className='font-semibold text-xl my-1 text-[#757575]'>{company_name}</p>
+                <div className='space-x-3'>
+                    <button className='btn bg-white hover:bg-white normal-case font-extrabold text-base rounded-lg border-2 py-2 px-4 text-[#7E90FE] border-[#7E90FE]'>{remote_or_onsite}</button>
+                    <button className='btn bg-white hover:bg-white normal-case font-extrabold text-base rounded-lg border-2 py-2 px-4 text-[#7E90FE] border-[#7E90FE]'>{job_type}</button>
                 </div>
-                <div>
-                    <p>
-                        <span>{location}</span>
-                        <span>{salary}</span>
-                    </p>
+
+                <div className='flex gap-4 my-2 font-semibold text-xl text-[#757575]'>
+                    <h2 className='flex gap-1 '><IoLocationOutline className='text-2xl text-[#757575]'></IoLocationOutline> {location}</h2>
+                    <h2 className='flex gap-1'><BiDollarCircle className='text-2xl'></BiDollarCircle> {salary}</h2>
                 </div>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+
+                <div className="card-actions">
+                    <button className="btn btn-primary normal-case bg-[#7E90FE] text-white hover:bg-[#9873FF] border-none font-extrabold  text-base">View Details</button>
                 </div>
             </div>
         </div>
