d02240ac138606857b200301fa813054fe39989a
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-12 12:21:16 +0600

    Nested Routes and footer with daisyUI

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './components/Root/Root'
+import Home from './components/Home/Home'
 
 
 
 
 const router = createBrowserRouter([
-  
-  {
-    path: '/',
-    element: <div>hello world</div>
-  },
+
+    {
+        path: "/",
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+
+            }
+        ]
+
+    },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>,
 )
