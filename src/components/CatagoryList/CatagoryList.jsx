9cc8ea8ae4a8cdb41a273aa4defadf5e515b7858
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-12 16:06:49 +0600

    work on job feature

--- src/components/CatagoryList/CatagoryList.jsx
+++ src/components/CatagoryList/CatagoryList.jsx
@@ -2,7 +2,10 @@
 const CatagoryList = () => {
     return (
         <div>
-            <h1>CatagoryList</h1>
+            <div className="text-center space-y-3 my-3">
+                <h1 className="text-5xl font-extrabold text-[#1A1919]">Job Category List</h1>
+                <p className="text-xl font-normal text-[#757575]">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
         </div>
     );
 };
--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -2,7 +2,10 @@
 const FeaturedJobs = () => {
     return (
         <div>
-            
+            <div className="text-center space-y-3 my-3">
+                <h1 className="text-5xl font-extrabold text-[#1A1919]">Featured Jobs</h1>
+                <p className="text-xl font-normal text-[#757575]">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
         </div>
     );
 };
